b77972782137815a63773db613f1d897a2b99224
gustasvs <jakensjake@gmail.com>
2024-03-27 15:03:11 +0200

    refracturing / ratings / improved order saving

---
+++ discord_utils/order_management_helpers.py
@@ -0,0 +1,67 @@
+import json
+
+ORDER_PATH = 'secret/orders.json'
+
+def save_order(new_order):
+    try:
+        with open(ORDER_PATH, 'r') as file:
+            orders = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        orders = {}
+
+    dish_id = new_order['dish-id']
+    
+    if dish_id in orders:
+        if not any(user['user-id'] == new_order['user-id'] and user['day-id'] == new_order['day-id'] for user in orders[dish_id]['consumers']):
+            orders[dish_id]['consumers'].append({
+                'user-id': new_order['user-id'],
+                'day-id': new_order['day-id']
+            })
+    else:
+        orders[dish_id] = {
+            'dish-title': new_order['dish-title'],
+            'dish-category-title': new_order['dish-category-title'],
+            'dish-price': new_order['dish-price'],
+            'dish-ratings': [],
+            'consumers': [{
+                'user-id': new_order['user-id'],
+                'day-id': new_order['day-id']
+            }]
+        }
+    with open(ORDER_PATH, 'w') as file:
+        json.dump(orders, file, indent=4)
+
+def rate_order(dish_id, user_id, rating):
+    try:
+        with open(ORDER_PATH, 'r') as file:
+            orders = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        orders = {}
+
+    if dish_id in orders:
+        orders[dish_id]['dish-ratings'].append({
+            'user-id': user_id,
+            'rating': rating
+        })
+
+    with open(ORDER_PATH, 'w') as file:
+        json.dump(orders, file, indent=4)
+
+def get_ratings():
+    try:
+        with open(ORDER_PATH, 'r') as file:
+            orders = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
+
+    output = {}
+    for dish_id, dish_info in orders.items():
+        for rating in dish_info['dish-ratings']:
+            output[rating].append(dish_info['dish-title'])
+
+    return output
+
+def save_todays_orders(orders):
+    # rewrites existing todays orders
+    with open('secret/todays_orders.json', 'w') as file:
+        json.dump(orders, file, indent=4)
