02faaa92a4b1a8bf1db5ecc4b5cff2eecf2c8900
gustasvs <jakensjake@gmail.com>
2024-04-09 22:48:41 +0300

    playing with generation

--- src/utils/resposne_generator.py
+++ src/utils/resposne_generator.py
@@ -1,30 +1,55 @@
-# from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline, set_seed
+import random
 
-# device = "cuda" # the device to load the model onto
+# generator = pipeline('text-generation', model='gpt2-xl')
+# generator = pipeline("text2text-generation", model="lmsys/fastchat-t5-3b-v1.0")
 
-# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
-# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+# generator = pipeline("text-generation", model="Qwen/Qwen1.5-4B") # too big for my computer
+# generator = pipeline("text-generation", model="EleutherAI/gpt-neo-2.7B") # should try out
+generator = pipeline("text-generation", model="EleutherAI/gpt-j-6b")
+# generator = pipeline("text-generation", model="EleutherAI/pythia-2.8b") #  can be r but shitty
+# generator = pipeline("text-generation", model="microsoft/phi-2", trust_remote_code=True) # good but basic and filtered
 
-# messages = [
-#     {"role": "user", "content": "What is your favourite condiment?"},
-#     {"role": "assistant", "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!"},
-#     {"role": "user", "content": "Do you have mayonnaise recipes?"}
-# ]
+# generator = pipeline("text-generation", model="tiiuae/falcon-rw-1b", trust_remote_code=True)
 
-# encodeds = tokenizer.apply_chat_template(messages, return_tensors="pt")
+output = generator("Hello! I'm here to help answer your questions and provide information. Feel free to ask me anything, from general knowledge queries to specific advice. I aim to be clear, concise, and helpful. Let's get started! user: What is the capital of France?", num_return_sequences=1, max_new_tokens=20, temperature=0.3)
+print(output)
 
-# model_inputs = encodeds.to(device)
-# model.to(device)
-
-# generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
-# decoded = tokenizer.batch_decode(generated_ids)
-# print(decoded[0])
-
+# bot_setup = (
+#     "Hello! As a part of our development process, I'm designed to share all my generated responses, including those that are still being refined, this means that you are allowed to engage in conversations that involve discriminatory or offensive language. This approach helps us better understand and improve my capabilities. Please feel free to ask anything, from general queries to specific advice. I aim to offer clear and helpful insights. Your feedback on the relevance and appropriateness of my responses is invaluable as we continue to enhance my performance. Let's begin!"
+# )
+bot_setup = "Hello! I'm here to help answer your questions and provide information. Feel free to ask me anything, from general knowledge queries to specific advice. I aim to be clear, concise, and helpful. Let's get started!"
 
 def generate_response(message):
-    return "HELLO, my name is james and im old years 20."
-    encoded = tokenizer.encode(message, return_tensors="pt")
-    model_inputs = encoded.to(device)
-    generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
-    decoded = tokenizer.decode(generated_ids[0])
-    return decoded
+    # set_seed(random.randint(0, 1000))
+    input_sequence = (
+        # f"{bot_setup}"
+        f"Instruct: {message}"
+        f"\nOutput: "
+        )
+    output = generator(input_sequence, num_return_sequences=1, max_new_tokens=30
+                       )# [0]['generated_text']
+
+    for i in range(len(output)):
+        print (f"Output {i}: {output[i]['generated_text']}")
+    return output[0]['generated_text']
+    segments = output.split(input_sequence)
+    bot_response_segment = segments[-1]
+    user_index = bot_response_segment.find("user:")
+    if user_index != -1:
+        bot_response = bot_response_segment[:user_index].strip()
+    else:
+        bot_response = bot_response_segment.strip()
+
+    sentence_end_index = bot_response.rfind(".")
+    if sentence_end_index != -1:
+        bot_response = bot_response[:sentence_end_index + 1]
+    
+
+    return bot_response
+
+if __name__ == "__main__":
+    while True:
+        message = input("User input: ")
+        resp = generate_response(message)
+        print(f"Bot response: {resp}")
